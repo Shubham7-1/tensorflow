f86013ec20040c696198b8ef7da39c2ba5b6fafb
Abhinav Gunjal <agunjal@google.com>
2025-04-23 12:05:51 -0700

    AllReduce,Case,If,Map,Reduce,Return,While : Direct StableHLO to HLO Translation.
    PiperOrigin-RevId: 750677250

--- third_party/xla/xla/mlir_hlo/mhlo/transforms/stablehlo_legalize_to_hlo/stablehlo_legalize_to_hlo_pass.cc
+++ third_party/xla/xla/mlir_hlo/mhlo/transforms/stablehlo_legalize_to_hlo/stablehlo_legalize_to_hlo_pass.cc
@@ -30,6 +30,7 @@ limitations under the License.
 #include "mlir/Support/LogicalResult.h"
 #include "mlir/Transforms/DialectConversion.h"
 #include "stablehlo/dialect/StablehloOps.h"
+#include "third_party/stablehlo/stablehlo/dialect/StablehloOps.h"
 
 namespace mlir {
 namespace mhlo {
@@ -67,7 +68,9 @@ void legalDirectStablehloToHloConversionOps(ConversionTarget& target) {
       stablehlo::TriangularSolveOp, stablehlo::XorOp, stablehlo::CreateTokenOp,
       stablehlo::TupleOp, stablehlo::SendOp, stablehlo::RecvOp,
       stablehlo::InfeedOp, stablehlo::OutfeedOp, stablehlo::GetTupleElementOp,
-      stablehlo::OptimizationBarrierOp>();
+      stablehlo::OptimizationBarrierOp, stablehlo::WhileOp, stablehlo::CaseOp,
+      stablehlo::IfOp, stablehlo::AllReduceOp, stablehlo::ReduceOp,
+      stablehlo::MapOp, stablehlo::ReturnOp>();
 }
 
 struct StablehloLegalizeToHloPass
